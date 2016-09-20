28febc97ffb8d3b1e402a5c122325381d4ac78eb
Josiah Atwell <1boy1dr@gmail.com>
2016-09-20 19:33:17 -0400

    initial commit

---
+++ init.el
@@ -0,0 +1,40 @@
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+;;don't use tab characters, they're evil and will murder your dog
+(setq-default indent-tabs-mode nil)
+
+;;my prefered indent length
+(setq c-basic-offset 4)
+
+;;my prefered coding style
+(setq c-default-style '((java-mode . "bsd")
+                        (other . "bsd")))
+
+;; show column number
+(setq column-number-mode t)
+
+;;crap, I can't even remember what this does, I'll just leave it I guess...
+(defun my-make-CR-do-indent ()
+  (define-key c-mode-base-map "\C-m" 'c-context-line-break))
+(add-hook 'c-initialization-hook 'my-make-CR-do-indent)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-archives
+   (quote
+    (("gnu" . "http://elpa.gnu.org/packages/")
+     ("melpa" . "https://melpa.org/packages/"))))
+ '(package-selected-packages (quote (auto-overlayscomplete dash))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
