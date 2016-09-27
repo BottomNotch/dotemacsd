e8ff47a0d5c7ed13ae7da030ad252d46cbdbd7f6
Josiah Atwell <1boy1dr@gmail.com>
2016-09-27 13:34:53 -0400

    update .gitignore to ignore epla/, update init.el to show line numbers and use evil-nerd-commenter package (also automatically install evil-nerd-commenter if it's not installed)

--- init.el
+++ init.el
@@ -1,7 +1,4 @@
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
+;;activate all the packages (in particular autoloads)
 (package-initialize)
 
 ;;don't use tab characters, they're evil and will murder your dog
@@ -9,6 +6,9 @@
 
 ;;my prefered indent length
 (setq c-basic-offset 4)
+
+;;show line numbers
+(global-linum-mode 1)
 
 ;;my prefered coding style
 (setq c-default-style '((java-mode . "bsd")
@@ -22,6 +22,18 @@
   (define-key c-mode-base-map "\C-m" 'c-context-line-break))
 (add-hook 'c-initialization-hook 'my-make-CR-do-indent)
 
+(evilnc-default-hotkeys)
+
+(add-hook 'c-mode-common-hook
+  (lambda ()
+    ;; Preferred comment style
+    (setq comment-start "// "
+          comment-end "")))
+
+; list wanted packages
+(setq package-list '(evil-nerd-commenter
+                     ))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -31,10 +43,19 @@
    (quote
     (("gnu" . "http://elpa.gnu.org/packages/")
      ("melpa" . "https://melpa.org/packages/"))))
- '(package-selected-packages (quote (auto-overlayscomplete dash))))
+ '(package-selected-packages (quote (evil-nerd-commenter auto-overlayscomplete dash))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;;fetch the list of packages available 
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;;install the missing packages
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
